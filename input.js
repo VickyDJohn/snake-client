105173533ad6298ab16797d4fe4e0f3df6198f27
Vicky John <vicky.d.john@email.com>
2023-05-18 21:26:30 -0700

    modified handleUserInput to write controls to the server and not console log the commands; also added a connection variable in the globoal scope to be used by handleUserInput

--- input.js
+++ input.js
@@ -1,4 +1,5 @@
-exports.setupInput = function() {
+exports.setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,8 +8,28 @@ exports.setupInput = function() {
   return stdin;
 };
 
-const handleUserInput = () => {
+//similar to using conn.write in 'client.js', use connection.write here to pass keyboard input
+const handleUserInput = (key) => {
+  if (key === 'w' || key === 'W') {
+    if (connection)
+      connection.write("Move: up\n");
+  }
+  if (key === 'a' || key === 'A') {
+    if (connection)
+      connection.write("Move: left\n");
+  }
+  if (key === 's' || key === 'S') {
+    if (connection)
+      connection.write("Move: down\n");
+  }
+  if (key === 'd' || key === 'D') {
+    if (connection)
+      connection.write("Move: right\n");
+  }
   if (key === '\u0003') {
-    process.exit;
+    console.log("Exiting...");
+    process.exit();
   }
 };
+
+let connection;
