95dcc4e5b6d462d3e5e0478c3bf6958cefb0a4f5
Vicky John <vicky.d.john@email.com>
2023-05-18 21:27:46 -0700

    added a variable connection to start the connection and passed this variable to setupInput so inputs can be sent to the server

--- play.js
+++ play.js
@@ -2,6 +2,8 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log("Connecting...");
-connect();
+//update variable to be used with connect function
+const connection = connect();
 
-setupInput();
+//pass variable to setupInput so that keyboard inputs can be sent to the server
+setupInput(connection);
