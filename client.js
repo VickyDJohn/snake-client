e4099195f2d4d956964992150da3aec19514dbbc
Vicky John <vicky.d.john@email.com>
2023-05-18 20:14:09 -0700

    Added setupInput to read keyboard inputs as well handleUserInput to exit the game when ctrl+c is pressed

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const net = require('net');
 
+//using shorthand 'exports', connect to the server
 exports.connect = () => {
   const conn = net.createConnection({
     host: '172.17.167.177',
@@ -8,21 +9,22 @@ exports.connect = () => {
 
   conn.setEncoding('utf8');
 
+  //use console.log to display messages and conn.write to send messages, controls to the server
   const connected = conn.on("connect", () => {
     console.log("Connected!");
     conn.write("Name: VDJ");
-    setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 1000);
     // setTimeout(() => {
-    //   conn.write("Move: left");
-    // }, 2000);
-    // setTimeout(() => {
-    //   conn.write("Move: down");
-    // }, 3000);
-    // setTimeout(() => {
-    //   conn.write("Move: right");
-    // }, 4000);
+    // //   conn.write("Move: up");
+    // // }, 1000);
+    // // setTimeout(() => {
+    // //   conn.write("Move: left");
+    // // }, 2000);
+    // // setTimeout(() => {
+    // //   conn.write("Move: down");
+    // // }, 3000);
+    // // setTimeout(() => {
+    // //   conn.write("Move: right");
+    // // }, 4000);
   });
 
   const endMsg = conn.on("data", (message) => {
