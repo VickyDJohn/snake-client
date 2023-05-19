e05aec18ca5284859fe00d1d365b258d613b50c4
Vicky John <vicky.d.john@email.com>
2023-05-18 21:46:04 -0700

    Refactored code by removing unnecessary code and added canned messages to displayed on the server

--- input.js
+++ input.js
@@ -11,22 +11,22 @@ exports.setupInput = (conn) => {
 //similar to using conn.write in 'client.js', use connection.write here to pass keyboard input
 const handleUserInput = (key) => {
   if (key === 'w' || key === 'W') {
-    if (connection)
-      connection.write("Move: up\n");
-  }
-  if (key === 'a' || key === 'A') {
-    if (connection)
-      connection.write("Move: left\n");
-  }
-  if (key === 's' || key === 'S') {
-    if (connection)
-      connection.write("Move: down\n");
-  }
-  if (key === 'd' || key === 'D') {
-    if (connection)
-      connection.write("Move: right\n");
-  }
-  if (key === '\u0003') {
+      connection.write("Move: up");
+  } else if (key === 'a' || key === 'A') {
+      connection.write("Move: left");
+  } else if (key === 's' || key === 'S') {
+      connection.write("Move: down");
+  } else if (key === 'd' || key === 'D') {
+      connection.write("Move: right");
+  } else if (key === '1') {
+    connection.write("Say: Too slow, slowpoke!");
+  } else if (key === '2') {
+    connection.write("Say: That was unfortunate :/");
+  } else if (key === '3') {
+    connection.write("Say: Well, well, well. Look who it is.");
+  } else if (key === '4') {
+    connection.write("Say: PogChamp!");
+  }else if (key === '\u0003') {
     console.log("Exiting...");
     process.exit();
   }
